f35c6d61356002b466c3f96d2cd6ba144cb589a8
toxidity-18 <desavigatunganga@gmail.com>
2026-01-06 10:50:00 +0300

    test_passed

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -2,6 +2,8 @@
 
 import ipdb
 
-def plus_two(num):
-    num + 2
+def plus_two(num): # 3 will come from the value of the test file .
+    ipdb.set_trace()
+    # num + 2
+    num = num + 2
     return num
--- lib/testing/ipdb_debugging_test.py
+++ lib/testing/ipdb_debugging_test.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-from ipdb_debugging import plus_two
+from ipdb_debugging import plus_two # this is used to find the function named plus_two inside the other file ipdb_debugging.py
 
-class TestIpdbDebugging:
+class TestIpdbDebugging: #container which keep all the related test together .
     '''ipdb_debugging.py'''
     
-    def test_adds_two(self):
+    def test_adds_two(self): # the mission or rather the test that pytest will run automatically .
         '''adds_two() adds 2 to input arg and returns sum.'''
-        assert(plus_two(3) == 5)
+        assert(plus_two(3) == 5) # assert is like a boolean either return true or false .
+
